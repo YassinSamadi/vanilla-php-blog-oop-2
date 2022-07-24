5790d1ba039df9e254854dd6f6db11e5dc316ab8
YassinSamadi <samadiyassin@hotmail.com>
2022-07-24 22:43:30 +0200

    added home page
    Home page include redirects, work in progress for registering

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <a  href="./pages/home.php">Home</a> | 
+    <a href="./pages/register.php">Register</a> | 
+    <a href="./pages/login.php">Login</a> 
+</head>
+<body>
+    
+</body>
+</html>
---
+++ pages/register.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <h1>Sign up</h1>
+    
+    <form method="POST">
+    <input type="text" placeholder="Name">
+    <input type="text" placeholder="email">
+    <input type="password" placeholder="Password">
+    <button type="submit" class="btn">Register</button>
+    </form>
+
+</head>
+<body>
+    
+</body>
+</html>
